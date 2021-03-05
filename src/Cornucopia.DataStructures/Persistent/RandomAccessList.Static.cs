a782f92cd5069f537c942f9dcbcb34c98beae5ed
Simon Weinberger <mail@sweini.de>
2021-03-05 19:59:54 +0100

    (Persistent) random access lists (based on skew binary numbers)

---
+++ src/Cornucopia.DataStructures/Persistent/RandomAccessList.Static.cs
@@ -0,0 +1,21 @@
+﻿namespace Cornucopia.DataStructures.Persistent
+{
+    public static class RandomAccessList
+    {
+        public static RandomAccessList<T> Create<T>(T item)
+        {
+            return RandomAccessList<T>.Empty.AddFirst(item);
+        }
+
+        public static RandomAccessList<T> Create<T>(params T[] items)
+        {
+            var result = RandomAccessList<T>.Empty;
+            for (var i = items.Length - 1; i >= 0; i--)
+            {
+                result = result.AddFirst(items[i]);
+            }
+
+            return result;
+        }
+    }
+}
