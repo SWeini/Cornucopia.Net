a89453b98ba4a4949ed500969eb9ea1fea2a4ca5
Simon Weinberger <mail@sweini.de>
2021-03-06 23:21:15 +0100

    cleanup

--- src/Cornucopia.DataStructures/Persistent/RandomAccessList.Static.cs
+++ src/Cornucopia.DataStructures/Persistent/RandomAccessList.Static.cs
@@ -1,12 +1,16 @@
-﻿namespace Cornucopia.DataStructures.Persistent
+﻿using System.Diagnostics.Contracts;
+
+namespace Cornucopia.DataStructures.Persistent
 {
     public static class RandomAccessList
     {
+        [Pure]
         public static RandomAccessList<T> Create<T>(T item)
         {
             return RandomAccessList<T>.Empty.AddFirst(item);
         }
 
+        [Pure]
         public static RandomAccessList<T> Create<T>(params T[] items)
         {
             var result = RandomAccessList<T>.Empty;
