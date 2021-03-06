afbcbaa4914ac997e53af217eb740ee5d458fa91
Simon Weinberger <mail@sweini.de>
2021-03-06 23:23:30 +0100

    Inline static methods in RandomAccessList

--- src/Cornucopia.DataStructures/Persistent/RandomAccessList.cs
+++ src/Cornucopia.DataStructures/Persistent/RandomAccessList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 namespace Cornucopia.DataStructures.Persistent
 {
@@ -7,32 +8,142 @@ namespace Cornucopia.DataStructures.Persistent
     {
         public static RandomAccessList<T> Empty => default;
 
-        private readonly LinkedList<Node>? root;
+        private readonly LinkedList<Node>? _root;
 
         private RandomAccessList(LinkedList<Node>? root)
         {
-            this.root = root;
+            this._root = root;
         }
 
-        public bool IsEmpty => this.root.IsEmpty();
+        public bool IsEmpty => this._root.IsEmpty();
 
-        public bool Any => this.root.Any();
+        public bool Any => this._root.Any();
 
-        public int Count() => Count(this.root);
+        [Pure]
+        public int Count()
+        {
+            var list = this._root;
+            var result = 0;
+            while (list.Any())
+            {
+                result += list.Head.Count;
+                list = list.Tail;
+            }
 
-        public T First => GetFirst(this.root);
+            return result;
+        }
 
-        public RandomAccessList<T> AddFirst(T value) => AddFirst(this.root, value);
+        public T First
+        {
+            get
+            {
+                this._root.EnsureNotEmpty();
+                return this._root.Head.Tree.Value;
+            }
+        }
 
-        public RandomAccessList<T> RemoveFirst(out T value) => RemoveFirst(this.root, out value);
+        [Pure]
+        public RandomAccessList<T> AddFirst(T value)
+        {
+            var first = this._root;
+            if (first.Any())
+            {
+                var second = first.Tail;
+                if (second.Any())
+                {
+                    if (first.Head.Count == second.Head.Count)
+                    {
+                        var combinedTree = new BinaryTree<T>(first.Head.Tree, second.Head.Tree, value);
+                        var combinedNode = new Node(combinedTree, first.Head.Count * 2 + 1);
+                        return new(second.Tail.Prepend(combinedNode));
+                    }
+                }
+            }
 
-        public T this[int index] => GetNthNode(this.root, index).Value;
+            var singleValueTree = BinaryTree.Create(value);
+            var singleValueNode = new Node(singleValueTree, 1);
+            return new(first.Prepend(singleValueNode));
+        }
 
-        public void ForEach(Action<T> action) => ForEach(this.root, action);
+        [Pure]
+        public RandomAccessList<T> RemoveFirst(out T value)
+        {
+            this._root.EnsureNotEmpty();
+            var tree = this._root.Head.Tree;
+            value = tree.Value;
+            if (this._root.Head.Count == 1)
+            {
+                return new(this._root.Tail);
+            }
+
+            var halfCount = this._root.Head.Count / 2;
+            return new(this._root
+                .Tail
+                .Prepend(new(tree.RightChild!, halfCount))
+                .Prepend(new(tree.LeftChild!, halfCount)));
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                var list = this._root;
+                while (list.Any())
+                {
+                    var node = list.Head;
+                    if (index >= node.Count)
+                    {
+                        index -= node.Count;
+                        list = list.Tail;
+                        continue;
+                    }
+
+                    var tree = node.Tree;
+                    var count = node.Count / 2;
+                    while (true)
+                    {
+                        if (index == 0)
+                        {
+                            return tree.Value;
+                        }
+
+                        index--;
+                        if (index < count)
+                        {
+                            tree = tree.LeftChild!;
+                        }
+                        else
+                        {
+                            tree = tree.RightChild!;
+                            index -= count;
+                        }
+
+                        count /= 2;
+                    }
+                }
+
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            var list = this._root;
+            while (list.Any())
+            {
+                BinaryTree.ForEachPreOrder(list.Head.Tree, action);
+                list = list.Tail;
+            }
+        }
 
         public IEnumerator<T> GetEnumerator()
         {
-            var list = this.root;
+            var list = this._root;
             while (list.Any())
             {
                 var node = list.Head;
@@ -55,116 +166,6 @@ namespace Cornucopia.DataStructures.Persistent
                         todo = todo.Prepend(tree.LeftChild);
                     }
                 }
-            }
-        }
-
-        private static int Count(LinkedList<Node>? list)
-        {
-            var result = 0;
-            while (list.Any())
-            {
-                result += list.Head.Count;
-                list = list.Tail;
-            }
-
-            return result;
-        }
-
-        private static T GetFirst(LinkedList<Node>? root)
-        {
-            root.EnsureNotEmpty();
-            return root.Head.Tree.Value;
-        }
-
-        private static RandomAccessList<T> AddFirst(LinkedList<Node>? root, T value)
-        {
-            var first = root;
-            if (first.Any())
-            {
-                var second = first.Tail;
-                if (second.Any())
-                {
-                    if (first.Head.Count == second.Head.Count)
-                    {
-                        var combinedTree = new BinaryTree<T>(first.Head.Tree, second.Head.Tree, value);
-                        var combinedNode = new Node(combinedTree, first.Head.Count * 2 + 1);
-                        return new(second.Tail.Prepend(combinedNode));
-                    }
-                }
-            }
-
-            var singleValueTree = BinaryTree.Create(value);
-            var singleValueNode = new Node(singleValueTree, 1);
-            return new(first.Prepend(singleValueNode));
-        }
-
-        private static RandomAccessList<T> RemoveFirst(LinkedList<Node>? node, out T value)
-        {
-            node.EnsureNotEmpty();
-            var tree = node.Head.Tree;
-            value = tree.Value;
-            if (node.Head.Count == 1)
-            {
-                return new(node.Tail);
-            }
-
-            var halfCount = node.Head.Count / 2;
-            return new(node
-                .Tail
-                .Prepend(new(tree.RightChild!, halfCount))
-                .Prepend(new(tree.LeftChild!, halfCount)));
-        }
-
-        private static BinaryTree<T> GetNthNode(LinkedList<Node>? list, int index)
-        {
-            if (index < 0)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-
-            while (list.Any())
-            {
-                var node = list.Head;
-                if (index >= node.Count)
-                {
-                    index -= node.Count;
-                    list = list.Tail;
-                    continue;
-                }
-
-                var tree = node.Tree;
-                var count = node.Count / 2;
-                while (true)
-                {
-                    if (index == 0)
-                    {
-                        return tree;
-                    }
-
-                    index--;
-                    if (index < count)
-                    {
-                        tree = tree.LeftChild!;
-                    }
-                    else
-                    {
-                        tree = tree.RightChild!;
-                        index -= count;
-                    }
-
-                    count /= 2;
-                }
-            }
-
-            throw new ArgumentOutOfRangeException();
-        }
-
-        private static void ForEach(LinkedList<Node>? list, Action<T> action)
-        {
-            while (list.Any())
-            {
-                BinaryTree.ForEachPreOrder(list.Head.Tree, action);
-                list = list.Tail;
             }
         }
 
