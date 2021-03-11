257685d738aa18ba8216ec3ded81e711a4ce5523
Simon Weinberger <mail@sweini.de>
2021-03-11 20:04:40 +0100

    xml docs for LinkedList

--- src/Cornucopia.DataStructures/Persistent/LinkedList.Static.cs
+++ src/Cornucopia.DataStructures/Persistent/LinkedList.Static.cs
@@ -4,26 +4,52 @@ using System.Diagnostics.Contracts;
 
 namespace Cornucopia.DataStructures.Persistent
 {
+    /// <summary>
+    ///     A set of methods for instances of <see cref="LinkedList{T}"/>.
+    /// </summary>
     public static class LinkedList
     {
+        /// <summary>
+        ///     Creates a list with the specified element as its only member.
+        /// </summary>
+        /// <typeparam name="T">The type of elements stored by the list.</typeparam>
+        /// <param name="value">The element to store in the list.</param>
+        /// <returns>A new list with the specified element.</returns>
         [Pure]
         public static LinkedList<T> Create<T>(T value)
         {
             return new(null, value);
         }
 
+        /// <summary>
+        ///     Gets a value indicating whether a list is empty.
+        /// </summary>
+        /// <typeparam name="T">The type of elements stored by the list.</typeparam>
+        /// <param name="list">The list to examine.</param>
+        /// <returns><c>true</c> if <paramref name="list"/> is empty; otherwise, <c>false</c>.</returns>
         [Pure]
         public static bool IsEmpty<T>([NotNullWhen(false)] this LinkedList<T>? list)
         {
             return list is null;
         }
 
+        /// <summary>
+        ///     Gets a value indicating whether a list has any elements.
+        /// </summary>
+        /// <typeparam name="T">The type of elements stored by the list.</typeparam>
+        /// <param name="list">The list to examine.</param>
+        /// <returns><c>true</c> if <paramref name="list"/> has any elements; otherwise, <c>false</c>.</returns>
         [Pure]
         public static bool Any<T>([NotNullWhen(true)] this LinkedList<T>? list)
         {
             return list is not null;
         }
 
+        /// <summary>
+        ///     Ensures that a list is not empty.
+        /// </summary>
+        /// <typeparam name="T">The type of elements stored by the list.</typeparam>
+        /// <param name="list">The list to examine.</param>
         public static void EnsureNotEmpty<T>([NotNull] this LinkedList<T>? list)
         {
             if (list.IsEmpty())
@@ -32,6 +58,12 @@ namespace Cornucopia.DataStructures.Persistent
             }
         }
 
+        /// <summary>
+        ///     Performs the specified action on each element of a list.
+        /// </summary>
+        /// <typeparam name="T">The type of elements stored by the list.</typeparam>
+        /// <param name="list">The list to process.</param>
+        /// <param name="action">The <see cref="Action{T}"/> delegate to perform on each element of the list.</param>
         public static void ForEach<T>(this LinkedList<T>? list, Action<T> action)
         {
             while (list.Any())
@@ -41,6 +73,12 @@ namespace Cornucopia.DataStructures.Persistent
             }
         }
 
+        /// <summary>
+        ///     Reverses the elements of a list.
+        /// </summary>
+        /// <typeparam name="T">The type of elements stored by the list.</typeparam>
+        /// <param name="list">The list to process.</param>
+        /// <returns>A list with reversed elements.</returns>
         [Pure]
         [return: NotNullIfNotNull("list")]
         public static LinkedList<T>? Reverse<T>(this LinkedList<T>? list)
@@ -60,12 +98,25 @@ namespace Cornucopia.DataStructures.Persistent
             return result;
         }
 
+        /// <summary>
+        ///     Prepends a specified element to a list.
+        /// </summary>
+        /// <typeparam name="T">The type of elements stored by the list.</typeparam>
+        /// <param name="list">The list to process.</param>
+        /// <param name="value">The element to prepend to <paramref name="list"/>.</param>
+        /// <returns>A new list with <paramref name="value"/> followed by <paramref name="list"/>.</returns>
         [Pure]
         public static LinkedList<T> Prepend<T>(this LinkedList<T>? list, T value)
         {
             return new(list, value);
         }
 
+        /// <summary>
+        ///     Counts the elements in a list.
+        /// </summary>
+        /// <typeparam name="T">The type of elements stored by the list.</typeparam>
+        /// <param name="list">The list to examine.</param>
+        /// <returns>The number of elements in <paramref name="list"/>.</returns>
         [Pure]
         public static int Count<T>(this LinkedList<T>? list)
         {
--- src/Cornucopia.DataStructures/Persistent/LinkedList.cs
+++ src/Cornucopia.DataStructures/Persistent/LinkedList.cs
@@ -1,7 +1,15 @@
 ﻿namespace Cornucopia.DataStructures.Persistent
 {
+    /// <summary>
+    ///     A persistent singly-linked list.
+    /// </summary>
+    /// <remarks>The value <c>null</c> is valid and represents an empty list.</remarks>
+    /// <typeparam name="T">The type of elements stored by the list.</typeparam>
     public sealed class LinkedList<T>
     {
+        /// <summary>
+        ///     The empty list.
+        /// </summary>
         public static LinkedList<T>? Empty => null;
 
         internal LinkedList(LinkedList<T>? tail, T head)
@@ -10,7 +18,14 @@
             this.Head = head;
         }
 
+        /// <summary>
+        ///     The list without the head element.
+        /// </summary>
         public LinkedList<T>? Tail { get; }
+
+        /// <summary>
+        ///     The element at the head of the list.
+        /// </summary>
         public T Head { get; }
     }
 }
