1f28eb691e2d772a3065f64664ce4d4e28eb8a1e
Simon Weinberger <mail@sweini.de>
2021-03-14 17:25:46 +0100

    BinaryHeap

---
+++ src/Cornucopia.DataStructures.Tests/BinaryHeapTests.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace Cornucopia.DataStructures
+{
+    [TestFixture]
+    public class BinaryHeapTests
+    {
+        [Test]
+        public void Ctor_DefaultComparer_CountIsZero()
+        {
+            var heap = new BinaryHeap<int>(Comparer<int>.Default);
+            Assert.That(heap.Count, Is.Zero);
+        }
+
+        [Test]
+        public void Ctor_Capacity_CountIsZero()
+        {
+            var heap = new BinaryHeap<int>(42, Comparer<int>.Default);
+            Assert.That(heap.Count, Is.Zero);
+        }
+
+        [Test]
+        public void Ctor_NegativeCapacity_Throws()
+        {
+            Assert.That(() => new BinaryHeap<int>(-1, Comparer<int>.Default), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void Count_Empty_IsZero()
+        {
+            var heap = new BinaryHeap<int>();
+            Assert.That(heap.Count, Is.Zero);
+        }
+
+        [Test]
+        public void Insert_Empty_CountIsOne()
+        {
+            var heap = new BinaryHeap<int>();
+            heap.Insert(0);
+            Assert.That(heap.Count, Is.EqualTo(1));
+        }
+
+        [TestCase(5)]
+        [TestCase(42)]
+        public void Insert_MultipleTimesIntoEmpty_CountIsCorrect(int size)
+        {
+            var heap = new BinaryHeap<int>();
+            for (var i = 0; i < size; i++)
+            {
+                heap.Insert(0);
+            }
+
+            Assert.That(heap.Count, Is.EqualTo(size));
+        }
+
+        [Test]
+        public void Minimum_Empty_Throws()
+        {
+            var heap = new BinaryHeap<int>();
+            Assert.That(() => heap.Minimum, Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Minimum_SingleElement_ReturnsElement()
+        {
+            var heap = new BinaryHeap<int>();
+            heap.Insert(42);
+            Assert.That(heap.Minimum, Is.EqualTo(42));
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(2, 1, 3)]
+        [TestCase(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0)]
+        public void Minimum_ManyElements_IsCorrect(params int[] values)
+        {
+            var heap = new BinaryHeap<int>();
+            foreach (var value in values)
+            {
+                heap.Insert(value);
+            }
+
+            Assert.That(heap.Minimum, Is.EqualTo(values.Min()));
+        }
+
+        [Test]
+        public void ExtractMinimum_Empty_Throws()
+        {
+            var heap = new BinaryHeap<int>();
+            Assert.That(() => heap.ExtractMinimum(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void ExtractMinimum_SingleElement_ReturnsElement()
+        {
+            var heap = new BinaryHeap<int>();
+            heap.Insert(42);
+            Assert.That(heap.ExtractMinimum(), Is.EqualTo(42));
+        }
+
+        [Test]
+        public void ExtractMinimum_SingleElement_RemovesElement()
+        {
+            var heap = new BinaryHeap<int>();
+            heap.Insert(0);
+            heap.ExtractMinimum();
+            Assert.That(heap.Count, Is.Zero);
+            Assert.That(() => heap.Minimum, Throws.InvalidOperationException);
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(2, 1, 3)]
+        [TestCase(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0)]
+        [TestCase(5, 3, 4, 11, 8, 15)]
+        public void ExtractMinimum_ManyElements_CanSortSequence(params int[] values)
+        {
+            var heap = new BinaryHeap<int>();
+            foreach (var value in values)
+            {
+                heap.Insert(value);
+            }
+
+            var sorted = new List<int>();
+            while (heap.Count > 0)
+            {
+                sorted.Add(heap.ExtractMinimum());
+            }
+
+            Assert.That(sorted, Is.EqualTo(values.OrderBy(x => x).ToList()));
+        }
+    }
+}
---
+++ src/Cornucopia.DataStructures/BinaryHeap.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Cornucopia.DataStructures
+{
+    /// <summary>
+    ///     A min-heap
+    /// </summary>
+    /// <typeparam name="T">The type of elements to store in the heap.</typeparam>
+    public class BinaryHeap<T>
+    {
+        private readonly IComparer<T> _comparer;
+        private DynamicArray<T> _elements;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BinaryHeap{T}"/> class that is empty and uses the default comparer.
+        /// </summary>
+        public BinaryHeap()
+        {
+            this._comparer = Comparer<T>.Default;
+            this._elements = new DynamicArray<T>(true);
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BinaryHeap{T}"/> class that is empty.
+        /// </summary>
+        /// <param name="comparer">The comparer used to compare elements.</param>
+        public BinaryHeap(IComparer<T> comparer)
+        {
+            this._comparer = comparer;
+            this._elements = new DynamicArray<T>(true);
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BinaryHeap{T}"/> class with a specified initial capacity.
+        /// </summary>
+        /// <param name="capacity">The initial capacity of the heap.</param>
+        /// <param name="comparer">The comparer used to compare elements.</param>
+        public BinaryHeap(int capacity, IComparer<T> comparer)
+        {
+            this._comparer = comparer;
+            this._elements = new DynamicArray<T>(capacity);
+        }
+
+        /// <summary>
+        ///     Gets the number of elements contained in the heap.
+        /// </summary>
+        /// <value>The number of elements in the heap.</value>
+        public int Count => this._elements.Length;
+
+        /// <summary>
+        ///     Inserts an element into the heap.
+        /// </summary>
+        /// <param name="item">The element to insert into the heap.</param>
+        public void Insert(T item)
+        {
+            var idx = this._elements.Length;
+            this._elements.AddLast(item);
+            BinaryHeapUtilies.Up(this._elements.AsSpan(), idx, this._comparer);
+        }
+
+        /// <summary>
+        ///     A minimum element contained in the heap.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The heap is empty.</exception>
+        public T Minimum
+        {
+            get
+            {
+                if (this._elements.Length == 0)
+                {
+                    throw new InvalidOperationException("The heap is empty.");
+                }
+
+                return this._elements[0];
+            }
+        }
+
+        /// <summary>
+        ///     Extracts a minimum element from the heap.
+        /// </summary>
+        /// <returns>A minimum element that was contained in the heap.</returns>
+        /// <exception cref="InvalidOperationException">The heap is empty.</exception>
+        public T ExtractMinimum()
+        {
+            if (!this.TryExtractMinimum(out var result))
+            {
+                throw new InvalidOperationException("The heap is empty.");
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        ///     Extracts a minimum element from the heap.
+        /// </summary>
+        /// <param name="result">A minimum element that was contained in the heap.</param>
+        /// <returns><c>true</c> if an element was removed from the heap; otherwise, <c>false</c>.</returns>
+        public bool TryExtractMinimum([MaybeNullWhen(false)] out T result)
+        {
+            if (this._elements.Length == 0)
+            {
+                result = default;
+                return false;
+            }
+
+            result = this.RemoveAt(0);
+            return true;
+        }
+
+        private T RemoveAt(int idx)
+        {
+            var item = this._elements[idx];
+            var lastItem = this._elements.RemoveLast();
+            if (idx < this._elements.Length)
+            {
+                this._elements[idx] = lastItem;
+                BinaryHeapUtilies.Down(this._elements.AsSpan(), idx, this._comparer);
+            }
+
+            return item;
+        }
+    }
+}
