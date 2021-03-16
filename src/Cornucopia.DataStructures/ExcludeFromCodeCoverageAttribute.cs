0cd1e460beb721c839284b4966441df2c57a9198
Simon Weinberger <mail@sweini.de>
2021-03-16 20:39:17 +0100

    cleanup some code

--- src/Cornucopia.DataStructures/ExcludeFromCodeCoverageAttribute.cs
+++ src/Cornucopia.DataStructures/ExcludeFromCodeCoverageAttribute.cs
@@ -3,13 +3,9 @@
 // ReSharper disable once CheckNamespace
 namespace System.Diagnostics.CodeAnalysis
 {
-    [AttributeUsageAttribute(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Event, Inherited = false, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Event, Inherited = false)]
     internal sealed class ExcludeFromCodeCoverageAttribute : Attribute
     {
-        public ExcludeFromCodeCoverageAttribute()
-        {
-        }
-
         /// <summary>Gets or sets the justification for excluding the member from code coverage.</summary>
         public string? Justification { get; set; }
     }
