b26f8775ab82e756b3d851f46b5ad7ca0961dbb4
Simon Weinberger <mail@sweini.de>
2021-03-21 23:55:10 +0100

    JetBrains.Annotations.Extractor.Tests and a little bit of refactoring

---
+++ src/JetBrains.Annotations.Extractor.Tests/AnnotationCreatorTests.cs
@@ -0,0 +1,47 @@
+using System.IO;
+using System.Xml;
+
+using NUnit.Framework;
+
+namespace JetBrains.Annotations.Extractor.Tests
+{
+    [TestFixture]
+    public class AnnotationCreatorTests
+    {
+        [Test]
+        public void ProcessAssembly_ThisAssembly_Works()
+        {
+            var creator = new AnnotationCreator();
+            creator.ProcessAssembly(this.GetType().Assembly);
+            using (var writer = new StringWriter())
+            {
+                creator.Document.Save(writer);
+                var str = writer.ToString();
+                Assert.That(str, Is.EqualTo(ThisAssemblyAnnotations));
+            }
+        }
+
+        private const string ThisAssemblyAnnotations = @"<?xml version=""1.0"" encoding=""utf-16""?>
+<assembly name=""JetBrains.Annotations.Extractor.Tests"">
+  <member name=""M:JetBrains.Annotations.Extractor.Tests.FooAnnotated.Run(System.Collections.Generic.IEnumerable{System.Int32})"">
+    <attribute ctor=""M:JetBrains.Annotations.PureAttribute.#ctor"" />
+    <parameter name=""enumerable"">
+      <attribute ctor=""M:JetBrains.Annotations.InstantHandleAttribute.#ctor"" />
+    </parameter>
+  </member>
+  <member name=""M:JetBrains.Annotations.Extractor.Tests.FooAnnotated.Format(System.String,System.Object[])"">
+    <attribute ctor=""M:JetBrains.Annotations.StringFormatMethodAttribute.#ctor(System.String)"">
+      <argument>format</argument>
+    </attribute>
+  </member>
+  <member name=""M:JetBrains.Annotations.Extractor.Tests.FooAnnotated.AssertTrue(System.Boolean)"">
+    <attribute ctor=""M:JetBrains.Annotations.AssertionMethodAttribute.#ctor"" />
+    <parameter name=""condition"">
+      <attribute ctor=""M:JetBrains.Annotations.AssertionConditionAttribute.#ctor(JetBrains.Annotations.AssertionConditionType)"">
+        <argument>0</argument>
+      </attribute>
+    </parameter>
+  </member>
+</assembly>";
+    }
+}
---
+++ src/JetBrains.Annotations.Extractor.Tests/FooAnnotated.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JetBrains.Annotations.Extractor.Tests
+{
+    public class FooAnnotated
+    {
+        [Pure]
+        public int Run([InstantHandle] IEnumerable<int> enumerable)
+        {
+            return enumerable.Max();
+        }
+
+        [StringFormatMethod("format")]
+        public string Format(string format, object?[] args)
+        {
+            return string.Format(format, args);
+        }
+
+        [AssertionMethod]
+        public void AssertTrue([AssertionCondition(AssertionConditionType.IS_TRUE)] bool condition)
+        {
+        }
+    }
+}
